6e14ad950e03469902b87f4fd162638442f9b866
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-11-15 22:45:16 +0200

    Add Task6.cpp

---
+++ LaboratoryWork4/Task6/Task6.cpp
@@ -0,0 +1,28 @@
+#include <array>
+#include <iostream>
+
+/// Надрукувати елемнт.
+template <size_t Size>
+void printElement(const std::array<int, Size> &array, size_t index) {
+  std::cout << ' ' << array[index];
+
+  if (++index < Size)
+    printElement(array, index);
+}
+
+/// Надрукувати масив.
+template <size_t Size>
+void printArray(const std::array<int, Size> &array) {
+  std::cout << "Array:";
+
+  printElement(array, 0);
+  std::cout << '\n' << '\n';
+}
+
+int main() {
+  const size_t size = 10;
+  std::array<int, size> array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  printArray(array);
+  return 0;
+}
