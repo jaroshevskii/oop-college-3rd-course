1882b8c7d5d21accc88c0e4b7658eb7b88629092
jaroshevskii <jaroshevskiisasha@gmail.com>
2022-01-24 00:23:41 +0200

    Update laboratory work 1

---
+++ LaboratoryWorks/LaboratoryWork1/Task2/Task2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int getNumber() {
+  std::cout << "> ";
+  int number;
+  std::cin >> number;
+  return 0;
+}
+
+int getSumOfDigits(int number) {
+  int sumOfDigits = 0;
+
+  while (number) {
+    sumOfDigits += number % 10;
+    number /= 10;
+  }
+  return sumOfDigits;
+}
+
+int getLargestNumberBySumOfDigits() {
+  std::cout << "Enter integers (use 0 to end the entry).\n";
+
+  int largestNumber;
+  int sumOfDigitsOfLargestNumber;
+
+  int number;
+  int sumOfDigits;
+
+  while (true) {
+    number = getNumber();
+
+    if (number == 0) {
+      std::cout << '\n';
+      return largestNumber;
+    }
+
+    sumOfDigits = getSumOfDigits(number);
+
+    if (sumOfDigits > sumOfDigitsOfLargestNumber) {
+      sumOfDigitsOfLargestNumber = sumOfDigits;
+      largestNumber = number;
+    }
+  }
+}
+
+int main() {
+  const int largestNumberBySumOfDigits = getLargestNumberBySumOfDigits();
+  std::cout << "Largest number by sum of digits: " << largestNumberBySumOfDigits
+            << "\n\n";
+  return 0;
+}
