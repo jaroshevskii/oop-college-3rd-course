3c689ef22806ee25784002f862501bde5ef2268b
jaroshevskii <jaroshevskiisasha@gmail.com>
2022-04-28 18:55:11 +0300

    Update task 3

--- laboratory-works/laboratory-work2/task3/Main.cpp
+++ laboratory-works/laboratory-work2/task3/Main.cpp
@@ -2,6 +2,28 @@
 #include <iostream>
 #include <random>
 
+/// Returns the int number from the user in the range from min to max.
+int getIntFromUser(int min, int max) {
+  int number{};
+
+  while (true) {
+    std::cout << "> ";
+    std::cin >> number;
+
+    if (number >= min && number <= max) {
+      return number;
+    }
+
+    if (number < min) {
+      std::cerr << "error: The number entered must be greater than or equal to "
+                << min << ".";
+    } else if (number > max) {
+      std::cerr << "error: The number entered must be less than or equal to "
+                << max << ".";
+    }
+  }
+}
+
 /// Returns a random int number in the range from min to max.
 int getRandomInt(int min, int max) {
   std::mt19937 generator{std::random_device{}()};
@@ -9,56 +31,61 @@ int getRandomInt(int min, int max) {
   return distribution(generator);
 }
 
-/// Print array.
+/// Prints array.
 template <typename Type, size_t Size>
-void printArray(const std::array<Type, Size> &array) {
-  std::cout << "Array:";
+void printArray(const std::array<Type, Size> &array,
+                const std::string &name = "Array",
+                const std::string &terminator = "\n") {
+  std::cout << name << ":\n";
 
   for (const auto &element : array) {
-    std::cout << ' ' << element;
+    std::cout << "  " << element << '\n';
   }
-  std::cout << "\n\n";
+  std::cout << terminator;
 }
 
 int main() {
-  const int randomMin{-10};
-  const int randomMax{10};
+  std::cout << "Enter the max and min number to fill the numbers with random "
+               "values in "
+               "this range.\n";
 
-  std::cout << "Random min:  " << randomMin << '\n'
-            << "Random max:  " << randomMax << "\n\n";
+  using integer = std::numeric_limits<int>;
+  const auto randomMin = getIntFromUser(integer::min(), integer::max() - 1);
+  const auto randomMax = getIntFromUser(integer::min() + 1, integer::max());
 
-  std::array<int, 12> array{};
+  std::cout << '\n';
 
-  // Fill in the array.
-  for (auto &element : array) {
-    element = getRandomInt(randomMin, randomMax);
+  std::array<int, 12> numbers{};
+
+  // Fill in the numbers with random values.
+  for (auto &number : numbers) {
+    number = getRandomInt(randomMin, randomMax);
   }
 
-  printArray(array);
+  printArray(numbers, "Numbers");
 
   int indexMin{0};
   int indexMax{0};
 
-  // Find the index of the min and max element of the array.
-  for (int i = 1; i < array.size(); ++i) {
-    if (array[i] < array[indexMin])
+  // Finds the index of the min and max number in numbers.
+  for (int i{1}; i < numbers.size(); ++i) {
+    if (numbers[i] < numbers[indexMin]) {
       indexMin = i;
-    if (array[i] > array[indexMax])
+    } else if (numbers[i] > numbers[indexMax])
       indexMax = i;
   }
 
-  std::cout << "Index min:  " << indexMin << '\n'
-            << "Index max:  " << indexMax << "\n\n";
+  std::cout << "Index min: " << indexMin << '\n'
+            << "Index max: " << indexMax << "\n\n";
 
-  if (array[indexMin] == array[indexMax]) {
+  if (numbers[indexMin] == numbers[indexMax]) {
     std::cout << "No need to swap the min and max elements. Elements are "
                  "the same.\n\n";
   } else {
-    std::swap(array[indexMin], array[indexMax]);
-
+    std::swap(numbers[indexMin], numbers[indexMax]);
     std::cout << "Min and max element are swapped.\n\n";
 
-    printArray(array);
+    printArray(numbers, "Numbers", "");
   }
   return 0;
 }
