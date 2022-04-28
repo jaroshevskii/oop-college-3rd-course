d70367742ad874b36c17ba0878316a2e4f82c667
jaroshevskii <jaroshevskiisasha@gmail.com>
2022-04-28 18:54:35 +0300

    Update task 1

--- laboratory-works/laboratory-work2/task1/Main.cpp
+++ laboratory-works/laboratory-work2/task1/Main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 #include <random>
+#include <string>
 
 /// Returns a random int number in the range from min to max.
 int getRandomInt(int min, int max) {
@@ -17,37 +18,35 @@ double getRandomDouble(double min, double max) {
   return distribution(generator);
 }
 
-/// Returns an array element.
-double getArrayElement(int index) {
+/// Returns some number on the specified index.
+double getSomeNumber(int index) {
   switch (index) {
   case 0:
-    return getRandomInt(-4, 0);
+    return static_cast<double>(getRandomInt(-4, 0));
   case 1:
-    return getRandomInt(100, 299);
+    return static_cast<double>(getRandomInt(100, 299));
   case 2:
     while (true) {
-      const auto element = getRandomInt(-35, 1);
-      if (element % 2 == 0) {
-        return element;
+      if (const auto element = getRandomInt(-35, 1); element % 2 == 0) {
+        return static_cast<double>(element);
       }
     }
   case 3:
-    return getRandomInt(-128, 127);
+    return static_cast<double>(getRandomInt(-128, 127));
   case 4:
     while (true) {
-      const auto element = getRandomInt(-7, 12);
-      if (element % 2 != 0) {
-        return element;
+      if (const auto element = getRandomInt(-7, 12); element % 2 != 0) {
+        return static_cast<double>(element);
       }
     }
   case 5:
-    return getRandomDouble(-7.85, 28.0 * sqrt(3.0));
+    return getRandomDouble(-7.85, 28.0 * std::sqrt(3.0));
   case 6:
-    return getRandomInt(-100, 100);
+    return static_cast<double>(getRandomInt(-100, 100));
   case 7:
-    return getRandomInt(23, 71);
+    return static_cast<double>(getRandomInt(23, 71));
   case 8:
-    return getRandomInt(0, 1);
+    return static_cast<double>(getRandomInt(0, 1));
   case 9:
     return getRandomDouble(sqrt(17.0), sqrt(182.0));
   default:
@@ -57,25 +56,27 @@ double getArrayElement(int index) {
   }
 }
 
-/// Print array.
+/// Prints array.
 template <typename Type, size_t Size>
-void printArray(const std::array<Type, Size> &array) {
-  std::cout << "Array:\n";
+void printArray(const std::array<Type, Size> &array,
+                const std::string &name = "Array",
+                const std::string &terminator = "\n") {
+  std::cout << name << ":\n";
 
   for (const auto &element : array) {
     std::cout << "  " << element << '\n';
   }
-  std::cout << '\n';
+  std::cout << terminator;
 }
 
 int main() {
-  std::array<double, 10> array{};
+  std::array<double, 10> numbers{};
 
-  // Fill in the array.
-  for (int i = 0; i < array.size(); ++i) {
-    array[i] = getArrayElement(i);
+  // Fill in the numbers.
+  for (int i{0}; i < numbers.size(); ++i) {
+    numbers[i] = getSomeNumber(i);
   }
 
-  printArray(array);
+  printArray(numbers, "Numbers", "");
   return 0;
 }
